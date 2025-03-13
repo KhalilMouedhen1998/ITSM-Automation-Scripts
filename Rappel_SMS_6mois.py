8a87e3cceae900b25ebac2e381611770e43bd968
KhalilMouedhen1998 <khalil.mouedhen@gmail.com>
2025-03-13 11:00:53 +0100

    Rappel_SMS_6mois.py

--- Rappel_SMS_6mois.py
+++ Rappel_SMS_6mois.py
@@ -2,21 +2,21 @@
 #This script requires requests module installed in python.
 import requests
  
-url = "https://service-desk.focus-corporation.com/api/v3/requests/10972/notifications"
-headers ={"authtoken":"906B26D6-70E5-4B71-BAF8-DCD21143B0EE"}
+url = "/api/v3/requests/10972/notifications"
+headers ={"authtoken":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
 input_data = '''{
     "notification": {
         "subject": "Rappel : Vérification de l'abonnement au fournisseur de SMS",
         "description": "Bonjour, <br><br> Ceci est un rappel pour vérifier l'abonnement auprès du fournisseur de SMS.<br> <br>Cordialement,<br>Support FOCUS",
         "to": [
             {
-                "email_id": "khalil.mouadhen@focus-corporation.com"
+                "email_id": ""
             },
             {
-                "email_id": "khalil.naoui@focus-corporation.com"
+                "email_id": ""
             },
             {
-                "email_id": "medhoussein.sayah@focus-corporation.com"
+                "email_id": ""
             }
 
         ],
